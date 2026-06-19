20d4d4f70b1c3507365e5d4aa55d2fd355ea25b2
Sai Charan Kondagadapa <k.saicharan0047j@gmail.com>
2026-06-19 15:13:47 +0530

    completed the asteroid game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,43 @@
 extends Control
 
+@onready var selector = $Selector
+@onready var labels = [
+	$VBoxContainer/AsteroidvoidLabel,
+	$VBoxContainer/TankDuelLabel,
+	$VBoxContainer/PulseSurvivalLabel,
+	$VBoxContainer/QuitLabel
+]
+var selected = 0
+
+var menu_y = [260.0, 320.0,380.0,440.0]
+
 func _ready():
-	$VBoxContainer/StartButton.pressed.connect(_on_start_pressed)
-	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+	update_selector()
 	
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/asteroid_void.tscn")
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_down"):
+		selected += 1
+	if event.is_action_pressed("ui_up"):
+		selected -= 1
+		
+	selected = clamp(selected,0,3)
+	update_selector()
+	if event.is_action_pressed("ui_accept"):
+		launch_game()
+
+func update_selector():
+
+	var target = labels[selected]
+
+	selector.global_position.y = target.global_position.y
 	
-func _on_quit_pressed():
-	get_tree().quit()
+func launch_game():
+	match selected:
+		0:
+			get_tree().change_scene_to_file("res://scenes/asteroid_void.tscn")
+		1:
+			print("Tank Duel")
+		2:
+			print("pulse Survival")
+		3:
+			get_tree().quit()
