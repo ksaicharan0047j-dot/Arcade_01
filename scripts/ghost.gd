662dfb0481bf0c5c30a8bc979a8e20409ece53e6
Sai Charan Kondagadapa <k.saicharan0047j@gmail.com>
2026-07-10 13:19:46 +0530

    cooking

--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -1,11 +1,49 @@
 extends CharacterBody2D
 
-enum GhostType{
+enum GhostType {
 	BLINKY,
 	PINKY,
 	INKY,
 	CLYDE
 }
-
 @export var ghost_type := GhostType.BLINKY
-@export var ghost_color := Color.RED
+@onready var sprite: AnimatedSprite2D = $Body
+
+var look_dir := Vector2.RIGHT
+
+func _ready():
+	match ghost_type:
+		GhostType.BLINKY:
+			sprite.play("blinky")
+		GhostType.PINKY:
+			sprite.play("pinky")
+		GhostType.INKY:
+			sprite.play("inky")
+		GhostType.CLYDE:
+			sprite.play("clyde")
+
+func _draw():
+	draw_ghost_eyes()
+
+func draw_ghost_eyes():
+	var offset = look_dir * 1.6
+	draw_circle(
+		Vector2(-5,-7),
+		4,
+		Color.WHITE
+	)
+	draw_circle(
+		Vector2(5,-7),
+		4,
+		Color.WHITE
+	)
+	draw_circle(
+		Vector2(-5,-7) + offset,
+		1.5,
+		Color.BLUE
+	)
+	draw_circle(
+		Vector2(5,-7) + offset,
+		1.5,
+		Color.BLUE
+	)
